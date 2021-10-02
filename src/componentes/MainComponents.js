c4d483b7d423c01962b8eb43cc67bfdffe32cb18
Malcolm Lima <malcolmmellolima@gmail.com>
2021-10-02 20:10:26 -0300

    index, app e reducers feitos, pastas para componentes, paginas de navegação e componentes gerais tambem

---
+++ src/componentes/MainComponents.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Template = styled.div``
+
+export const PageContainter = styled.div``
+
+export const PageTitle = styled.h1``
+
+export const PageBody = styled.div``
