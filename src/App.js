fb90367bfe3afe8e3918f58fe2df497b6edc7259
Malcolm Lima <malcolmmellolima@gmail.com>
2021-10-02 20:07:55 -0300

    Index, app e reducers configurados; pastas de componentes, paginas para navegar e componentes que aparecem sempre independente da tela configurados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import './App.css';
+import React from 'react'
+import { connect } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
-function App() {
+import './App.css'
+
+import { Template } from './componentes/MainComponents'
+import Header from './componentes/partials/Header'
+import Footer from './componentes/partials/Footer'
+
+import Routes from './Routes'
+
+const Page = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Template>
+        <Header />
+        <Routes />
+        <Footer />
+      </Template>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user 
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Page)
